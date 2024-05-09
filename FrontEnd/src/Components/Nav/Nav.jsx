bf9c0d5c4eceefde2da591ff1fcad95e5016357a
amirshkur22 <amirshkurhu@gmail.com>
2024-05-09 21:30:51 +0300

    nav Updated

--- FrontEnd/src/Components/Nav/Nav.jsx
+++ FrontEnd/src/Components/Nav/Nav.jsx
@@ -4,36 +4,36 @@ import usaFlag from "../../assets/images/usaFlag.png";
 import { SlLocationPin } from "react-icons/sl";
 import { BsSearch } from "react-icons/bs";
 import { BiCart } from "react-icons/bi";
-import styles from "./nav.module.css";
+import navStyles from "./nav.module.css";
 const Nav = () => {
   return (
     <header>
-      <nav className={styles.navBarWrapper}>
-        <div className={styles.LeftSide}>
+      <nav className={navStyles.navBarWrapper}>
+        <div className={navStyles.LeftSide}>
           <a href="/">
             <img src={amazonLogo} alt="amazonLogo" width={100} />
           </a>
-          <div className={styles.SlLocationPin}>
+          <div className={navStyles.SlLocationPin}>
             <span>
               <SlLocationPin />
             </span>
-            <div className={styles.delivery}>
+            <div className={navStyles.delivery}>
               <p>Delivered to</p>
               <span>Ethiopia</span>
             </div>
           </div>
         </div>
         {/*  */}
-        <div className={styles.middlePart}>
+        <div className={navStyles.middlePart}>
           <select name="" id="">
             <option value="">All</option>
           </select>
           <input type="text" placeholder="Search Amazon" />
-          <BsSearch className={styles.BsSearch} size={27} />
+          <BsSearch className={navStyles.BsSearch} size={27} />
         </div>
         {/*  */}
-        <div className={styles.rightSide}>
-          <a className={styles.coutrySelection}>
+        <div className={navStyles.rightSide}>
+          <a className={navStyles.coutrySelection}>
             <img src={usaFlag} alt="" width={20} />
             <select name="" id="">
               <option value="">EN</option>
@@ -51,12 +51,12 @@ const Nav = () => {
             <span>& Orders</span>
           </a>
 
-          <div className={styles.lastFlex}>
-            <a href="" className={styles.verticalFlex}>
-              <span className={styles.cartCount}>0</span>
-              <BiCart className={styles.BiCart} size={35} />
+          <div className={navStyles.lastFlex}>
+            <a href="" className={navStyles.verticalFlex}>
+              <span className={navStyles.cartCount}>0</span>
+              <BiCart className={navStyles.BiCart} size={35} />
             </a>
-            <p className={styles.cart}>Cart</p>
+            <p className={navStyles.cart}>Cart</p>
           </div>
         </div>
       </nav>
