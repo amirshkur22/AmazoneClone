91b8a4e2579ff2206a1955b77667da60ed39fbdf
amirshkur22 <amirshkurhu@gmail.com>
2024-05-19 22:40:43 +0300

    commit for deploymen

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/AmazoneClone",
   server: {
     port: 3000,
     hmr: {
-      overlay:true
-    }
+      overlay: true,
+    },
   },
- 
 });
